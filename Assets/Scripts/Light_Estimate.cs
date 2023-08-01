ffc9e8f4af742bf09999defb11928d5d21ecf92b
poojapol01 <polpooja05@gmail.com>
2023-08-01 23:45:37 +0530

    Able to get and show Surrounding Light Values

--- Assets/Scripts/Light_Estimate.cs
+++ Assets/Scripts/Light_Estimate.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+using TMPro;
 
 public class Light_Estimate : MonoBehaviour
 {
     public ARCameraManager arCamMan;
     Light ourLight;
+    public TMP_Text brightness;
 
     private void OnEnable()
     {
@@ -25,5 +27,12 @@ public class Light_Estimate : MonoBehaviour
     private void GetLight(ARCameraFrameEventArgs args)
     {
         Debug.Log("Light Estimation: " +args.lightEstimation);
+        if (args.lightEstimation.mainLightColor.HasValue)
+        {
+            //brightness.text = $"Color Value: {args.lightEstimation.mainLightColor.Value}";
+            ourLight.color = args.lightEstimation.mainLightColor.Value;
+            float average_btightness = 0.2126f * ourLight.color.r + 0.7152f * ourLight.color.g + 0.0722f * ourLight.color.b;
+            brightness.text = average_btightness.ToString();
+        } 
     }
 }
